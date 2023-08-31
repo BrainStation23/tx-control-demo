eec43b8d631539aaa9714ff219989467f08c4d6f
Palash Kanti Bachar <palash924332931@gmail.com>
2023-08-31 15:28:34 +0600

    Project init...

---
+++ TxControlSubmitPopupDemo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+           return RedirectToAction("StaticReport");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult StaticReport()
+        
+        {
+            var test = $"{AppDomain.CurrentDomain.GetData("DataDirectory")}\\" + "Booking_Form_1_0_0.tx";
+            ViewData["DataPath"] = test;
+
+            return View();
+        }
+    }
+}
